c8d1e5d7763eb4de0aefe0c45d6fd3ee0122754d
Double <double@tanyu.mobi>
2023-11-16 09:53:38 +0800

    修改tailwind配置

--- tyCalendar_components/src/App.vue
+++ tyCalendar_components/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import calendarTop from "./components/calendarTop.vue";
 </script>
 
 <template>
+  <div class="mr-auto">
+    <calendarTop />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ tyCalendar_components/src/components/calendarTop.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+interface DataV {
+  year: number;
+  month: number;
+  day: number;
+}
+const data = reactive<DataV>({
+  day: 20,
+  month: 10,
+  year: 2023,
+});
+</script>
+
+<template>
+  <div class="container flex gap-20 h-6 border items-center">
+    <div class="font-bold cursor-pointer">&lt&lt</div>
+    <div class="font-bold cursor-pointer">&lt</div>
+    <div class="sele">{{ data.year }}</div>
+    <div class="font-bold cursor-pointer">&gt</div>
+    <div class="font-bold cursor-pointer">&gt&gt</div>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
