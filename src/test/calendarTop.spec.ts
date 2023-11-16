94db74bcdb101ea4a4bec00abef9114b6b20b6f1
Double <double@tanyu.mobi>
2023-11-16 13:20:19 +0800

    fix:新增测试用例

---
+++ src/test/calendarTop.spec.ts
@@ -0,0 +1,15 @@
+import { setYear } from "./../components/index";
+import { yearIncrease, yearDecrease } from "./../components/claendarTop";
+import { describe, test, expect } from "vitest";
+describe("日历头部相关", () => {
+  test("年份增长", () => {
+    let date = setYear({});
+    yearIncrease(date);
+    expect(date.year).toBe(2024);
+  });
+  test("年份减少", () => {
+    let date = setYear({});
+    yearDecrease(date);
+    expect(date.year).toBe(2022);
+  });
+});
